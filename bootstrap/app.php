a791ff12e426ef852ecc0bb425d20571fc21b7cb
cainscat <vulam3102@gmail.com>
2024-12-02 18:51:58 +0700

    Role management(admin)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\AdminMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,17 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // $middleware->append(AdminMiddleware::class);
+        $middleware->use([
+            \Illuminate\Foundation\Http\Middleware\InvokeDeferredCallbacks::class,
+            // \Illuminate\Http\Middleware\TrustHosts::class,
+            \Illuminate\Http\Middleware\TrustProxies::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
+            \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
+            \Illuminate\Http\Middleware\ValidatePostSize::class,
+            \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
+            \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Middleware\AdminMiddleware;
 
 Route::get('admin', [AuthController::class, 'login_admin']);
 Route::post('admin', [AuthController::class, 'auth_login_admin']);
@@ -10,12 +10,25 @@ Route::post('admin', [AuthController::class, 'auth_login_admin']);
 Route::get('admin/logout', [AuthController::class, 'logout_admin']);
 
 
-Route::get('admin/dashboard', function () {
-    return view('admin.dashboard');
-});
+// Route::group(['middleware' => 'admin'], function (){
+//     Route::get('admin/dashboard', function () {
+//         return view('admin.dashboard');
+//     });
 
-Route::get('admin/admin/list', function () {
-    return view('admin.admin.list');
+//     Route::get('admin/admin/list', function () {
+//         return view('admin.admin.list');
+//     });
+
+// });
+
+Route::middleware(AdminMiddleware::class)->group(function () {
+    Route::get('admin/dashboard', function () {
+        return view('admin.dashboard');
+    });
+
+    Route::get('admin/admin/list', function () {
+        return view('admin.admin.list');
+    });
 });
 
 Route::get('/', function (){
