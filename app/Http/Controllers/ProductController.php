a8398c20788040145b3c71e173db4ac0c00fcbc3
cainscat <vulam3102@gmail.com>
2024-12-10 12:29:06 +0700

    Category&Subcategory wise Product listing

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\CategoryModel;
 use App\Models\SubCategoryModel;
+use App\Models\ProductModel;
 
 class ProductController extends Controller
 {
@@ -21,6 +22,9 @@ class ProductController extends Controller
 
             $data['getSubCategory'] = $getSubCategory;
             $data['getCategory'] = $getCategory;
+
+            $data['getProduct'] = ProductModel::getProduct($getCategory->id,$getSubCategory->id);
+
             return view('product.list', $data);
         }
 
@@ -31,6 +35,8 @@ class ProductController extends Controller
             $data['meta_title'] = $getCategory->meta_title;
             $data['meta_description'] = $getCategory->meta_description;
             $data['meta_keywords'] = $getCategory->meta_keywords;
+
+            $data['getProduct'] = ProductModel::getProduct($getCategory->id);
 
             return view('product.list', $data);
         }
--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -20,9 +20,39 @@ class ProductModel extends Model
     {
         return self::select('product.*','users.name as created_by_name')
                 ->join('users', 'users.id', '=', 'product.created_by')
-                ->orderBy('product.id', 'desc')
                 ->where('product.is_delete', '=', 0)
-                ->paginate(20);
+                ->orderBy('product.id', 'desc')
+                ->paginate(50);
+    }
+
+    static public function getProduct($category_id = '', $subcategory_id = '')
+    {
+        $return = ProductModel::select('product.*','users.name as created_by_name',
+                'category.name as category_name', 'category.slug as category_slug',
+                'sub_category.name as sub_category_name', 'sub_category.slug as sub_category_slug')
+                ->join('users', 'users.id', '=', 'product.created_by')
+                ->join('category', 'category.id', '=', 'product.category_id')
+                ->join('sub_category', 'sub_category.id', '=', 'product.sub_category_id');
+
+                if(!empty($category_id))
+                {
+                    $return = $return->where('product.category_id', '=', $category_id);
+                }
+                if(!empty($subcategory_id))
+                {
+                    $return = $return->where('product.sub_category_id', '=', $subcategory_id);
+                }
+                $return = $return->where('product.is_delete', '=', 0)
+                                ->where('product.status', '=', 0)
+                                ->orderBy('product.id', 'desc')
+                                ->paginate(20);
+
+        return $return;
+    }
+
+    static public function getImageSingle($product_id)
+    {
+        return ProductImageModel::where('product_id', '=', $product_id)->orderBy('order_by', 'asc')->first();
     }
 
     static public function checkSlug($slug)
