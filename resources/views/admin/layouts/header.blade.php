f0b58c805bc6bcb02f4bd86a31ed3332e6a63a36
cainscat <vulam3102@gmail.com>
2025-01-13 19:42:27 +0700

    Up..

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -4,24 +4,6 @@
             <li class="nav-item"> <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button"> <i class="bi bi-list"></i> </a> </li>
         </ul>
         <ul class="navbar-nav ms-auto">
-            <li class="nav-item dropdown"> <a class="nav-link" data-bs-toggle="dropdown" href="#"> <i class="bi bi-chat-text"></i> <span class="navbar-badge badge text-bg-danger">1</span> </a>
-                {{-- <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end"> <a href="#" class="dropdown-item"> <!--begin::Message-->
-                        <div class="d-flex">
-                            <div class="flex-shrink-0"> <img src="{{ url('public/assets/dist/assets/img/user1-128x128.jpg') }}" alt="User Avatar" class="img-size-50 rounded-circle me-3"> </div>
-                            <div class="flex-grow-1">
-                                <h3 class="dropdown-item-title">
-                                    Brad Diesel
-                                    <span class="float-end fs-7 text-danger"><i class="bi bi-star-fill"></i></span>
-                                </h3>
-                                <p class="fs-7">Call me whenever you can...</p>
-                                <p class="fs-7 text-secondary"> <i class="bi bi-clock-fill me-1"></i> 4 Hours Ago
-                                </p>
-                            </div>
-                        </div>
-                    </a>
-                    <div class="dropdown-divider"></div> <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-                </div> --}}
-            </li>
             <li class="nav-item dropdown"> <a class="nav-link" data-bs-toggle="dropdown" href="#"> <i class="bi bi-bell-fill"></i> <span class="navbar-badge badge text-bg-warning">1</span> </a>
                 <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end"> <span class="dropdown-item dropdown-header">15 Notifications</span>
                     <div class="dropdown-divider"></div> <a href="#" class="dropdown-item"> <i class="bi bi-envelope me-2"></i> 4 new messages
@@ -41,15 +23,9 @@
 <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark"> <!--begin::Sidebar Brand-->
     <div class="sidebar-brand">
         <div class="brand-link">
-            <span class="brand-text fw-light" style="margin-left: 0px;">Ecommerce</span>
-        </div>
-    </div>
-    <div class="sidebar-brand">
-        <div class="user-panel mt-3 pb-3 d-flex">
-            <div class="image">
-                <img src="{{ url('public/assets/dist/assets/img/user2-160x160.jpg') }}" class="user-image rounded-circle shadow" style="max-width: 40px;" alt="User Image">
-                <span class="brand-text fw-light">{{ Auth::user()->name }}</span>
-            </div>
+            <span class="brand-text fw-light" style="margin-left: 0px;">
+                {{ Auth::user()->name }}
+            </span>
         </div>
     </div>
     <div class="sidebar-wrapper">
